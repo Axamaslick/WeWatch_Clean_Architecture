63055d55457efa8aea7830ab69ad62f352d5bc89
Lev <kartoxauaua@gmail.com>
2025-05-14 22:58:28 +0700

    Добавление ViewModel

--- app/src/main/java/com/sample/wewatch/presentation/MoviesViewModel.kt
+++ app/src/main/java/com/sample/wewatch/presentation/MoviesViewModel.kt
@@ -1,4 +1,35 @@
 package com.sample.wewatch.presentation
 
-class MoviesViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.sample.wewatch.domain.model.Movie
+import com.sample.wewatch.domain.usecase.AddMovieUseCase
+import com.sample.wewatch.domain.usecase.DeleteMovieUseCase
+import com.sample.wewatch.domain.usecase.GetAllMoviesUseCase
+import io.reactivex.disposables.CompositeDisposable
+
+class MoviesViewModel(
+    private val getAllMoviesUseCase: GetAllMoviesUseCase
+) : ViewModel() {
+
+    private val _movies = MutableLiveData<List<Movie>>()
+    val movies: LiveData<List<Movie>> = _movies
+
+    private val disposable = CompositeDisposable()
+
+    fun loadMovies() {
+        disposable.add(
+            getAllMoviesUseCase()
+                .subscribe(
+                    { moviesList -> _movies.postValue(moviesList) },
+                    { error -> /* обработка ошибки, например лог */ }
+                )
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
 }
